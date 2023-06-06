b6229179935ebe19ae0c8b837eef35e9981505d7
Jonathan Weeks <jonathan.weeks@pmi.com>
2023-06-06 20:58:22 +0100

    dobrze

---
+++ capture.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+
+def get_sequence(i):
+    s = str(i)
+    if len(s) == 1:
+        return '0' + s
+    else:
+        return s
+
+camera = cv2.VideoCapture("/dev/video2")
+i = 0
+while i < 60:
+    return_value, image = camera.read()
+    cv2.imwrite(f'/home/jon/temp/capture/image{get_sequence(i)}.png', image)
+    time.sleep(0.1)
+    i += 1
+del(camera)
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 A Python script to do Timelapse videos
+
+Make Video
+ffmpeg -framerate 10 -i image%02d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
